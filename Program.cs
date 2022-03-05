a57f29c1b0fd1f8fc5a1999fd06f3bb890f2b2bb
isabelle <e107ti@cps.sp.gov.br>
2022-03-04 21:52:23 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Cananéia");
+Console.WriteLine("Iguape");
+Console.WriteLine("Ilha Comprida");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Peruíbe");
+Console.WriteLine("Praia Grande");
